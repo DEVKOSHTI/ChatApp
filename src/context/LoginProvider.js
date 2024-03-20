56e3c3889ff8c1778a29d9ee8b7adeae6cd8a0ab
DEVKOSHTI <devkoshti0119@gmail.com>
2024-03-20 14:27:15 +0530

    login fixed

--- src/context/LoginProvider.js
+++ src/context/LoginProvider.js
@@ -1,9 +1,24 @@
-import React, { createContext, useContext, useState } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import React, { createContext, useContext, useEffect, useState } from 'react'
 
 const LoginContext = createContext()
 
 const LoginProvider = ({ children }) => {
     const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+    const fetchUser = async () => {
+        const uid = await AsyncStorage.getItem('UID');
+        if (uid != null) {
+            setIsLoggedIn(true)
+        } else {
+            setIsLoggedIn(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchUser()
+    }, [])
+
     return (
         <LoginContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
             {children}
